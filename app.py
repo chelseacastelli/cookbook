772c5310bd6331f00f3ee4769f4f06b26333c293
chelseacastelli <chel.castel@aol.com>
2021-12-15 17:00:14 -0500

    implement new comment form using FlaskForm

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 
 from flask import Flask, render_template, request
-from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions
+from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecret"
 
-@app.route('/')
+
+# Create form class
+class CommentForm(FlaskForm):
+  comment = StringField("Comment", validators=[DataRequired()])
+  submit = SubmitField("Add Comment")
+
+
+@app.route('/', methods=["GET"])
 def index():
   #### Return a rendered index.html file
   return render_template('index.html', template_recipes=recipes)
@@ -13,10 +24,17 @@ def index():
 def about():
   return render_template('about.html')
 
-@app.route("/recipe/<int:id>")
+@app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
+  # Instantiate form
+  comment_form = CommentForm(csrf_enabled=False)
+
+  if comment_form.validate_on_submit():
+    new_comment = comment_form.comment.data
+    comments[id].append(new_comment)
+
   #### Return a rendered fried_egg.html file
-  return render_template('recipe.html', template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id])
+  return render_template('recipe.html', template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
 
 @app.route('/new-recipe', methods=["GET", "POST"])
 def new_recipe():
@@ -31,5 +49,5 @@ def new_recipe():
     new_instructions = request.form["instructions"]
     add_ingredients(new_id, new_ingredients)
     add_instructions(new_id, new_instructions)
-    
+
   return render_template('new_recipe.html')
